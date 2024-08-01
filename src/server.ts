e21bfc4a3f78269c6214e6b453cfcb3578b22deb
williamlunelli <williamlunelli07@gmail.com>
2024-08-01 12:35:19 -0300

    base created

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import Express from "express";
+import dotenv from "dotenv";
+import mustacheExpress from "mustache-express";
+import path from "path";
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = Express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustacheExpress());
+
+server.use(Express.static(path.join(__dirname, '../public')))
+
+server.use(mainRoutes);
+
+server.use((req,res) =>{
+    res.send('Pagina não encontrada')
+})
+
+server.listen(process.env.PORT);
